8f0076a4ea6aeff0baf9888122e0f31726ba3a03
RobbeDaemen <robbe.daemen2@gmail.com>
2022-03-03 20:34:43 +0100

    Map relocated + added buttons

---
+++ js/map.js
@@ -0,0 +1,34 @@
+function showInformation(button) {
+    switch (button){
+        case 21:
+            document.getElementById("text-place").innerHTML = "This is button 21";
+            break;
+        case 22:
+            document.getElementById("text-place").innerHTML = "This is button 22";
+            break;
+        case 23:
+            document.getElementById("text-place").innerHTML = "This is button 23";
+            break;
+        case 24:
+            document.getElementById("text-place").innerHTML = "This is button 24";
+            break;
+        case 25:
+            document.getElementById("text-place").innerHTML = "This is button 25";
+            break;
+        case 26:
+            document.getElementById("text-place").innerHTML = "This is button 26";
+            break;
+        case 27:
+            document.getElementById("text-place").innerHTML = "This is button 27";
+            break;
+        case 28:
+            document.getElementById("text-place").innerHTML = "This is button 28";
+            break;
+        case 29:
+            document.getElementById("text-place").innerHTML = "This is button 29";
+            break;
+        case 30:
+            document.getElementById("text-place").innerHTML = "This is button 30";
+            break;
+    }
+}
